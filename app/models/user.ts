51347e877f79aa3023317f958e86d90b9c48d998
AlfieriChou <alfierichou@gmail.com>
2020-07-04 17:29:00 +0800

    fix: conflict

--- app/models/user.ts
+++ app/models/user.ts
@@ -2,21 +2,12 @@ import { ISchema } from '../../framework/types.ts'
 
 export default <ISchema>{
   model: {
-<<<<<<< HEAD
     id: { type: 'integer' },
     phone: { type: 'string', length: 11, description: '手机号' },
-=======
-    id: { type: 'integer', description: 'ID' },
-    phone: { type: 'string', description: '手机号' },
->>>>>>> 2a3e11036c7a716fa765ba08ad57dfe89874043b
     password: { type: 'string', length: 32, description: '密码' },
     nickname: { type: 'string', length: 32, description: '昵称' },
     createdAt: { type: 'date' },
     updatedAt: { type: 'date' },
     deletedAt: { type: 'date' }
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 2a3e11036c7a716fa765ba08ad57dfe89874043b
