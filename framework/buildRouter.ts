b53f4da89a22bb1d4868a7f033baef62168bbfde
AlfieriChou <alfierichou@gmail.com>
2020-07-05 10:13:26 +0800

    refactor: build router

--- framework/buildRouter.ts
+++ framework/buildRouter.ts
@@ -1,7 +1,22 @@
 import { Router, Context } from "https://deno.land/x/oak/mod.ts";
 import{ IRemoteMethod } from './types.ts'
 
-
+const controllerHandler = (modelName: string, handler: string) => {
+  return async (ctx: Context) => {
+    if (ctx.state.model[modelName] && ctx.state.model[modelName][handler]) {
+      ctx.response.body = {
+        message: 'success',
+        code: 0,
+        result: await ctx.state.model[modelName][handler]()
+      }
+    } else {
+      ctx.response.body = {
+        message: 'error',
+        code: 404
+      }
+    }
+  }
+}
 
 export default async (remoteMethods: { [s: string]: IRemoteMethod }, prefix: string = '/v1') => {
   const router = new Router()
@@ -17,68 +32,16 @@ export default async (remoteMethods: { [s: string]: IRemoteMethod }, prefix: str
     const { path, method } = remoteMethods[key]
     // TODO: validate roles, params and requestBody
     if (['get', 'GET'].includes(method)) {
-      router.get(`${prefix}${path}`, async (ctx: Context) => {
-        if (ctx.state.model[modelName] && ctx.state.model[modelName][handler]) {
-          ctx.response.body = {
-            message: 'success',
-            code: 0,
-            result: await ctx.state.model[modelName][handler]()
-          }
-        } else {
-          ctx.response.body = {
-            message: 'error',
-            code: 404
-          }
-        }
-      })
+      router.get(`${prefix}${path}`, controllerHandler(modelName, handler))
     }
     if (['post', 'POST'].includes(method)) {
-      router.post(`${prefix}${path}`, async (ctx: Context) => {
-        if (ctx.state.model[modelName] && ctx.state.model[modelName][handler]) {
-          ctx.response.body = {
-            message: 'success',
-            code: 0,
-            result: await ctx.state.model[modelName][handler]()
-          }
-        } else {
-          ctx.response.body = {
-            message: 'error',
-            code: 404
-          }
-        }
-      })
+      router.post(`${prefix}${path}`, controllerHandler(modelName, handler))
     }
     if (['put', 'PUT'].includes(method)) {
-      router.put(`${prefix}${path}`, async (ctx: Context) => {
-        if (ctx.state.model[modelName] && ctx.state.model[modelName][handler]) {
-          ctx.response.body = {
-            message: 'success',
-            code: 0,
-            result: await ctx.state.model[modelName][handler]()
-          }
-        } else {
-          ctx.response.body = {
-            message: 'error',
-            code: 404
-          }
-        }
-      })
+      router.put(`${prefix}${path}`, controllerHandler(modelName, handler))
     }
     if (['delete', 'DELETE'].includes(method)) {
-      router.delete(`${prefix}${path}`, async (ctx: Context) => {
-        if (ctx.state.model[modelName] && ctx.state.model[modelName][handler]) {
-          ctx.response.body = {
-            message: 'success',
-            code: 0,
-            result: await ctx.state.model[modelName][handler]()
-          }
-        } else {
-          ctx.response.body = {
-            message: 'error',
-            code: 404
-          }
-        }
-      })
+      router.delete(`${prefix}${path}`, controllerHandler(modelName, handler))
     }
   }, Promise.resolve())
   return router
